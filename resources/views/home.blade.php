37d31293b578b8fc2bf779a0c5b77d8b96146b18
ririswanris <w.ris1221@gmail.com>
2020-05-11 13:44:26 -0700

    darah

---
+++ resources/views/home.blade.php
@@ -0,0 +1,151 @@
+@extends('layouts.master')
+
+@section('top')
+@endsection
+
+@section('content')
+<!-- Small boxes (Stat box) -->
+<div class="row">
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+            <div class="inner">
+                <h3>{{ \App\User::count() }}</h3>
+
+                <p>Users</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-maroon">
+            <div class="inner">
+                <h3>{{ \App\Category::count() }}<sup style="font-size: 20px"></sup></h3>
+
+                <p>Category</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="{{ route('categories.index') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <!-- ./col -->
+    <!-- ./col -->
+</div>
+<div class="row">
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3>0</h3>
+
+                <p>Daftar Pendonor</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+            <a href=" " class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-navy">
+            <div class="inner">
+                <h3>0<sup style="font-size: 20px"></sup></h3>
+
+                <p>Kantong penggunaan</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href=" " class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-maroon">
+            <div class="inner">
+                <h3>0</h3>
+
+                <p>Pengguna Darah</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-person-add"></i>
+            </div>
+            <a href=" " class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-gray">
+            <div class="inner">
+                <h3>0</h3>
+
+                <p>Informasi</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href=" " class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+</div>
+
+    <div class="row">
+        <div class="box">
+            <div class="box-body">
+            <div class="callout callout-success">
+                <h4>Success</h4>
+
+                <p>{{ session('status') }} You are logged in!</p>
+            </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('top')
+@endsection
+
+
+
+
+
+
+
+
+
+{{--@extends('layouts.app')--}}
+
+{{--@section('content')--}}
+{{--<div class="container">--}}
+    {{--<div class="row justify-content-center">--}}
+        {{--<div class="col-md-8">--}}
+            {{--<div class="card">--}}
+                {{--<div class="card-header">Dashboard</div>--}}
+
+                {{--<div class="card-body">--}}
+                    {{--@if (session('status'))--}}
+                        {{--<div class="alert alert-success" role="alert">--}}
+                            {{--{{ session('status') }}--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
+
+                    {{--You are logged in!--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+{{--@endsection--}}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('dashboard', function () {
+   return view('layouts.master');
+});
+
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('categories','CategoryController');
+    Route::get('/apiCategories','CategoryController@apiCategories')->name('api.categories');
+    Route::get('/exportCategoriesAll','CategoryController@exportCategoriesAll')->name('exportPDF.categoriesAll');
+    Route::get('/exportCategoriesAllExcel','CategoryController@exportExcel')->name('exportExcel.categoriesAll');
+
+});
+
